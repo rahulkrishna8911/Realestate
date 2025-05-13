accca6c8c3137fe62624537bbaf0d8168d9fbbe1
Rahulkrishna <rahulkrishna8911@gmail.com>
2025-05-13 18:28:19 +0530

    static changes 2

--- Realestate/settings.py
+++ Realestate/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 import os
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "Static")]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'Static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
